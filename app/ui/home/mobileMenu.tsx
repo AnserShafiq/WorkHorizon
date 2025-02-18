a099f1d2882531ae3fc666293fcd3a16fd356a58
AnserShafiq <ansershafiq@gmail.com>
2025-02-19 02:31:22 +0500

    Feb 19,2025: Added Logo icon in mobile menu

--- app/ui/home/mobileMenu.tsx
+++ app/ui/home/mobileMenu.tsx
@@ -1,5 +1,6 @@
 'use client'
-import { Cross, Grip } from 'lucide-react';
+import { Grip, X } from 'lucide-react';
+import Image from 'next/image';
 import { useState } from 'react';
 
 
@@ -7,12 +8,13 @@ export default function MobileMenu() {
     const [openMenu, setOpenMenu] = useState<boolean>(false);
     // console.log(menuCall)
     return !openMenu ? (<Grip onClick={() => setOpenMenu(!openMenu)} />) : (
-        <div className='w-full z-50 h-full fixed top-0 left-0 bg-transparent-one'>
-            <Cross />
-            <div className='w-[88%] h-full flex flex-col items-end justify-end bg-sidemenu-bg'>
-                Side 
-
-            </div>
+        <div className='w-full z-40 h-full fixed top-0 left-0 bg-transparent-one'>
+            <X  className='absolute top-2 right-4 z-50' onClick={() => setOpenMenu(!openMenu)}/> 
+            <div className='w-[88%] h-full flex flex-col items-start justify-between bg-sidemenu-bg'>
+                <div>
+                    <Image src={'/assets/logo.png'} alt='Work horizon mobile logo' className='w-full h-full' width={200} height={100}/>
+                </div>
+            </div>  
             
         </div>
     )
