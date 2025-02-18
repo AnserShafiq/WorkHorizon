129b1f2984cad2099790a6fa04fd3e3d49d3015c
AnserShafiq <ansershafiq@gmail.com>
2025-02-19 02:14:14 +0500

    Feb 19,2025: Started to develop a job board website for Work Horizon

--- app/ui/home/mobileMenu.tsx
+++ app/ui/home/mobileMenu.tsx
@@ -10,7 +10,8 @@ export default function MobileMenu() {
         <div className='w-full z-50 h-full fixed top-0 left-0 bg-transparent-one'>
             <Cross />
             <div className='w-[88%] h-full flex flex-col items-end justify-end bg-sidemenu-bg'>
-                Side menu
+                Side 
+
             </div>
             
         </div>
