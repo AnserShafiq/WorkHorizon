a63d20339ec839d1ed4898deb53f913465b08356
AnserShafiq <ansershafiq@gmail.com>
2025-02-24 20:03:18 +0500

    Feb 24, 2025: Edited the screen sizes and widths

--- app/ui/header.tsx
+++ app/ui/header.tsx
@@ -50,9 +50,9 @@ export default function MainHeader() {
     return (
         <div className={`w-full bg-headerBackground py-2 shadow-sm shadow-gray-300 sticky top-0 z-50 transition-transform ease-in-out duration-500 transform ${headerDisplay ? 'translate-y-0' : '-translate-y-full'}`}>
             <div className="flex justify-between px-2 lg:px-0 lg:grid lg:grid-cols-[20%,60%,20%] container">
-                <div className="flex border">
+                <div className="flex">
                     <Link className="hidden lg:block" href={'/'}>
-                        <Image src={'/assets/Logo.png'} className="w-[400px] h-auto" alt='Work Horizon' width={400} height={200} />
+                        <Image src={'/assets/Logo.png'} className="w-auto h-auto" alt='Work Horizon' width={300} height={200} />
                     </Link>
                     <Link className="block lg:hidden" href={'/'}>
                         <Image src={'/assets/Logo.png'} className="h-auto w-auto" alt='Work Horizon' width={175} height={100} />
