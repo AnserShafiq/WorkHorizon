236720a5016865db7d0d80436e278ec47ea702c6
AnserShafiq <ansershafiq@gmail.com>
2025-02-25 02:40:43 +0500

    Feb 25, 2025: Added the black sub pages

---
+++ app/about-us/page.tsx
@@ -0,0 +1,8 @@
+
+export default function AboutUs(){
+    return(
+        <div className='container w-[88%] lg:w-full'>
+            About Work Horizon
+        </div>
+    )
+}
---
+++ app/contact-us/page.tsx
@@ -0,0 +1,9 @@
+
+
+export default function ContactUs(){
+    return(
+        <div className="container w-[88%] lg:w-full">
+            Contact Us Page
+        </div>
+    )
+}
