1bdf50d71da81d0f3c05745ea93ef62c2fe74901
AnserShafiq <ansershafiq@gmail.com>
2025-02-25 20:02:47 +0500

    Feb 25,2025: Working on about us

--- app/about-us/page.tsx
+++ app/about-us/page.tsx
@@ -1,8 +1,9 @@
+import PageIntro from "../ui/about-us/section1";
 
 export default function AboutUs(){
     return(
         <div className='container w-[88%] lg:w-full'>
-            About Work Horizon
+            <PageIntro />
         </div>
     )
 }
