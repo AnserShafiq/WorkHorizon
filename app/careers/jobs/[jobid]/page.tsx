e78340c8da976bd232e68e8f79bb339754e64ee0
AnserShafiq <ansershafiq@gmail.com>
2025-03-05 19:21:46 +0500

    March 05,2025: Added search section in the Jobs page

--- app/careers/jobs/[jobid]/page.tsx
+++ app/careers/jobs/[jobid]/page.tsx
@@ -1,21 +1,27 @@
 import { JobOptions } from "@/app/lib/joboptions";
 
-export default function Page({ params }: { params: { jobid: string } }) {
-    const { jobid } = params;
+interface PageProps {
+  params: {
+    jobid: string;
+  };
+}
 
-    const matchedJobs = JobOptions.filter((job) => job.jobid === jobid);
+export default function Page({ params }: PageProps) {
+  const { jobid } = params;
 
-    return (
-        <div className="container w-[88%] lg:w-full">
-            {matchedJobs.length > 0 ? (
-                matchedJobs.map((job, index) => (
-                    <div key={index} className="flex flex-col">
-                        <h3>{job.title}</h3>
-                    </div>
-                ))
-            ) : (
-                <h3>No job found</h3>
-            )}
-        </div>
-    );
+  const matchedJobs = JobOptions.filter((job) => job.jobid === jobid);
+
+  return (
+    <div className="container w-[88%] lg:w-full">
+      {matchedJobs.length > 0 ? (
+        matchedJobs.map((job, index) => (
+          <div key={index} className="flex flex-col">
+            <h3>{job.title}</h3>
+          </div>
+        ))
+      ) : (
+        <h3>No job found</h3>
+      )}
+    </div>
+  );
 }
