f4baa05fa022ac0344eab76a58290f2b7de2c1ca
AnserShafiq <ansershafiq@gmail.com>
2025-03-21 01:39:36 +0500

    March 21, 2025: Developing portal's dashboard

--- app/portal/dashboard/page.tsx
+++ app/portal/dashboard/page.tsx
@@ -1,11 +1,10 @@
 'use client'
 import { User } from '@/app/lib/elements';
 import DashboardBody from '@/app/ui/portal/dashboard';
-import { signOut } from 'next-auth/react';
 import { useEffect, useState } from 'react';
 
 export default function Dashboard() {
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+
     const [user, setUser] = useState<User | null>(null);
     useEffect(() => {
         const fetchUser = async () => {
