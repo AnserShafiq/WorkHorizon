604d5e5bd29dc48b1fd4c2ff4b8b82729f975711
AnserShafiq <ansershafiq@gmail.com>
2025-03-28 19:20:31 +0500

    March 28,2025: Project got completed.

--- app/api/message/deleteMessage/route.tsx
+++ app/api/message/deleteMessage/route.tsx
@@ -6,7 +6,7 @@ export async function DELETE(request: NextRequest){
     try {
         const id = await request.json()
         // await executeQuery('DELETE FROM messages WHERE id=? ',[id])
-        return NextResponse.json({message: 'Message got deleted successfully'},{status: 200});
+        return NextResponse.json({message: `Message got deleted successfully, ${id}`},{status: 200});
     } catch (error) {
         console.log('Unable to deleted message from DB, ',error)
         return NextResponse.json({message: 'Unable to deleted message from DB.'},{status: 404});
